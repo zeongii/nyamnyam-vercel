4265a83aa2a16bc3b550123068f7a17d4a0a47e0
zeongii <yaksanjy@naver.com>
2024-10-16 18:23:27 +0900

    update api user

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -19,7 +19,7 @@ interface User {
 
 export default function Header() {
   const { openModalWishlist } = useModalWishlistContext();
-  const { setSearchTerm } = useSearchContext(); 
+  const { setSearchTerm } = useSearchContext();
   const [user, setUser] = useState<User | null>(null);
   const router = useRouter();
 
@@ -64,7 +64,7 @@ export default function Header() {
 
   const handleHomeClick = () => {
     setSearchTerm('');
-    router.push('/'); 
+    router.push('/');
   };
 
   return (
@@ -102,10 +102,10 @@ export default function Header() {
                   </Link>
               )}
               {user?.role === 'ADMIN' && (
-              <Link href="/admin/dash" className="action-btn">
-                <Icon.LegoSmiley size={40} />
-                <span className="animation-ripple-delay2"></span>
-              </Link>
+                  <Link href="/admin/dash" className="action-btn">
+                    <Icon.LegoSmiley size={40} />
+                    <span className="animation-ripple-delay2"></span>
+                  </Link>
               )}
               <Link href="/chatRoom" className="action-btn">
                 <i className="ico_message"></i>
@@ -116,10 +116,10 @@ export default function Header() {
                 <span className="animation-ripple-delay2"></span>
               </Link>
               {user ? (
-              <Link href={`/user/mypage/${user?.userId}`} className="profile">
-                <img src="/assets/img/profile.png" alt="profile" />
-              </Link>
-                  ) : null}
+                  <Link href={`/user/mypage/${user?.userId}`} className="profile">
+                    <img src="/assets/img/profile.png" alt="profile" />
+                  </Link>
+              ) : null}
               <button onClick={handleOpenModal} className="action-btn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="black" viewBox="0 0 256 256">
                   <path d="M178,40c-20.65,0-38.73,8.88-50,23.89C116.73,48.88,98.65,40,78,40a62.07,62.07,0,0,0-62,62c0,70,103.79,126.66,108.21,129a8,8,0,0,0,7.58,0C136.21,228.66,240,172,240,102A62.07,62.07,0,0,0,178,40ZM128,214.8C109.74,204.16,32,155.69,32,102A46.06,46.06,0,0,1,78,56c19.45,0,35.78,10.36,42.6,27a8,8,0,0,0,14.8,0c6.82-16.67,23.15-27,42.6-27a46.06,46.06,0,0,1,46,46C224,155.61,146.24,204.15,128,214.8Z"></path>
