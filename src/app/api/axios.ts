a80d1459804f1f660f74f6cce299ab0733c93fab
zeongii <yaksanjy@naver.com>
2024-11-04 21:56:20 +0900

    update

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 export const instance  = axios.create ({
-    baseURL : process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL || "http://localhost:8080", 
+    baseURL : process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL || "https://abc.nyamnyam.kr",
 });
 
 
