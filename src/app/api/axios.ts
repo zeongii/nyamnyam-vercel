1ad052a0df39bfa53f2654dbc2541743ddbc4a5d
JEunseo <jjeseo1010@gmail.com>
2024-11-06 15:41:13 +0900

    axios withcredential add

--- src/app/api/axios.ts
+++ src/app/api/axios.ts
@@ -4,10 +4,12 @@ import axios from "axios";
 
 export const instance  = axios.create ({
     baseURL : process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL || "http://localhost:8080", 
+    withCredentials: true,
 }); 
 
 export const instance1  = axios.create ({
     baseURL : process.env.NEXT_PUBLIC_REACT_APP_SERVER_URL || "http://localhost:8081", 
+    withCredentials: true,
 }); 
 
 export default instance;
